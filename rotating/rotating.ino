9e67ea3085e76a027f7f0db380f3b1e402281c14
Sean Fish <seantfish@gmail.com>
2021-01-31 01:46:28 -0500

    Got phototransistors working with pullup

--- rotating/rotating.ino
+++ rotating/rotating.ino
@@ -1,34 +1,38 @@
-// Rotating- Implementation Goal: Switch between R/C and autonomous and drive motor and servo
-// TODO - Get hardware... Currently Pi3 and Arduino Nano. Read commands via Serial read
-// VERY MUCH NOT READY TO GO
+// Hasbro R2D2 Quadrature encoder based dome actuator
 
+// PWR - Brown
+// GND - Purple
+// Internal pull-up resistors appear well tuned for this. Tried 1M Ohm resistor for pull-down, got minimal success.
 
 // Arduino pin numbers
-const byte BLUE_PIN = A0;
-const byte YELLOW_PIN = A2;
-const byte GREEN_PIN = A4;
+const byte QUAD1_PIN = A0; // Red Wire
+const byte QUAD2_PIN = A2; // Grey Wire
+const byte CENT_PIN = A4; // Blue Wire
 
 // Variables
-double blue_val = -99999;
-double yellow_val = -99999;
-double green_val = -99999;
+double quad1_val = -99999;
+double quad2_val = -99999;
+double cent_val = -99999;
 
 
 void setup() {
   Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+  pinMode(QUAD1_PIN, INPUT_PULLUP);
+  pinMode(QUAD2_PIN, INPUT_PULLUP);
+  pinMode(CENT_PIN, INPUT_PULLUP);
 }
 
 void loop() {
   // Check instructions from Pi (Mode, Drive data)
-  blue_val = analogRead(BLUE_PIN);
-  yellow_val = analogRead(YELLOW_PIN);
-  green_val = analogRead(GREEN_PIN);
-  Serial.print("Blue   - ");
-  Serial.println(blue_val);
-  Serial.print("Yellow - ");
-  Serial.println(yellow_val);
-  Serial.print("Green  - ");
-  Serial.println(green_val);
+  quad1_val = digitalRead(QUAD1_PIN);
+  quad2_val = digitalRead(QUAD2_PIN);
+  cent_val = digitalRead(CENT_PIN);
+  Serial.print("Quad 1  - ");
+  Serial.println(quad1_val);
+  Serial.print("Quad 2  - ");
+  Serial.println(quad2_val);
+  Serial.print("Center  - ");
+  Serial.println(cent_val);
   Serial.println();
   delay(200);
 }
